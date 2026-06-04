fd0c544247072ff1932d823ee5696afae36bc91f
dsmr linux <dsmr19961@gmail.com>
2026-06-03 23:11:12 -0500

    refactor: cambiar MatrixRandom de clase estatica a funciones libres

--- include/BitMth/random/MatrixRandom.hpp
+++ include/BitMth/random/MatrixRandom.hpp
@@ -11,38 +11,39 @@ namespace Bitmth{
         inline std::mt19937 gen(rd());
 
         template <typename T>
-        class MatrixRandom {            
-            public:
+        void uniform(Matrix<T>& matrix, T min, T max){
+            std::uniform_real_distribution<T> dist(min, max);
+            for (size_t i = 0; i < matrix.numElements; i++) matrix.m[i] = dist(gen);
+        }
 
-                static void uniform(Matrix<T>& matrix, T min, T max){
-                    std::uniform_real_distribution<T> dist(min, max);
-                    for (size_t i = 0; i < matrix.numElements; i++) matrix.m[i] = dist(gen);
-                }
+        template <typename T>
+        void normal(Matrix<T>& matrix, T mean, T stddev){
+            std::normal_distribution<T> dist(mean, stddev);
+            for (size_t i = 0; i < matrix.numElements; i++) matrix.m[i] = dist(gen);
+        }
 
-                static void normal(Matrix<T>& matrix, T mean, T stddev){
-                    std::normal_distribution<T> dist(mean, stddev);
-                    for (size_t i = 0; i < matrix.numElements; i++) matrix.m[i] = dist(gen);
-                }
+        template <typename T>
+        void xavierUniform(Matrix<T>& matrix, size_t fanIn, size_t fanOut){
+            T d = sqrt( T(6.0) /(fanIn + fanOut) );
+            uniform( matrix, -d ,d );
+        }
 
-                static void xavierUniform(Matrix<T>& matrix, size_t fanIn, size_t fanOut){
-                    T d = sqrt( T(6.0) /(fanIn + fanOut) );
-                    uniform( matrix, -d ,d );
-                }
+        template <typename T>
+        void xavierNormal(Matrix<T>& matrix, size_t fanIn, size_t fanOut){
+            T stddev = sqrt( T(2.0) / (fanIn + fanOut) );
+            normal( matrix, T(0.0), stddev);
+        }
 
-                static void xavierNormal(Matrix<T>& matrix, size_t fanIn, size_t fanOut){
-                    T stddev = sqrt( T(2.0) / (fanIn + fanOut) );
-                    normal( matrix, T(0.0), stddev);
-                }
+        template <typename T>
+        void heUniform(Matrix<T>& matrix, size_t fanIn){
+            T d = sqrt( T(6.0) / fanIn );
+            uniform( matrix, -d ,d );
+        }
 
-                static void heUniform(Matrix<T>& matrix, size_t fanIn){
-                    T d = sqrt( T(6.0) / fanIn );
-                    uniform( matrix, -d ,d );
-                }
-
-                static void heNormal(Matrix<T>& matrix, size_t fanIn){
-                    T stddev = sqrt( T(2.0) / fanIn );
-                    normal( matrix, T(0.0), stddev);
-                }
-        };
+        template <typename T>
+        void heNormal(Matrix<T>& matrix, size_t fanIn){
+            T stddev = sqrt( T(2.0) / fanIn );
+            normal( matrix, T(0.0), stddev);
+        }
     }
 }
