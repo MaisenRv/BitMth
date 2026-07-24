3917f03404a89c3499453ad5fa31c62e8729edaa
dsmr linux <dsmr19961@gmail.com>
2026-07-23 19:58:32 -0500

    refactor(ParallelExecutor): replace function pointers with atomic types for thread safety and improve worker loop logic

--- include/BitMth/core/ParallelExecutor.hpp
+++ include/BitMth/core/ParallelExecutor.hpp
@@ -10,10 +10,12 @@
 namespace BitMth::core {
   class ParallelExecutor{
     private:
+      using FunctPtr = void (*)(void*, unsigned int, unsigned int);
+      using FunctMPtr = void (*)(void*, unsigned int, unsigned int, unsigned int, unsigned int);
 
-      void* tempTaskData = nullptr;
-      void (*tempFunct)(void* , unsigned int, unsigned int) = nullptr;
-      void (*tempFunctM)(void* , unsigned int, unsigned int, unsigned int, unsigned int) = nullptr;
+      std::atomic<void*> tempTaskData{nullptr};
+      std::atomic<FunctPtr> tempFunct{nullptr};
+      std::atomic<FunctMPtr> tempFunctM{nullptr};
 
       unsigned int numThreads;
       std::unique_ptr<Worker[]> workers;
@@ -22,35 +24,57 @@ namespace BitMth::core {
       void _workerLoop(unsigned int threadId){
         while (running.load(std::memory_order_relaxed)) {
 
-          while (!workers[threadId].hasWork.load(std::memory_order_acquire) && running.load(std::memory_order_relaxed)) {
+          while (!workers[threadId].hasWork.load(std::memory_order_acquire)) {
+            if (!running.load(std::memory_order_relaxed)) return;
             std::this_thread::yield();
           }    
 
-          if(!running.load(std::memory_order_relaxed)) break;
+          auto* f1 = tempFunct.load(std::memory_order_acquire);
+          auto* f2 = tempFunctM.load(std::memory_order_acquire);
+          void* data = tempTaskData.load(std::memory_order_acquire);
 
-          if (this->tempFunct != nullptr) {
-            this->tempFunct(tempTaskData, workers[threadId].start,workers[threadId].end);
-          }else if(this->tempFunctM != nullptr){
-            this->tempFunctM(tempTaskData,
+          if (f1 != nullptr) {
+            f1(data, workers[threadId].start,workers[threadId].end);
+          }else if(f2 != nullptr){
+            f2(data,
               workers[threadId].rowStart,workers[threadId].rowEnd,  
               workers[threadId].colStart,workers[threadId].colEnd  
             );
           }
-          workers[threadId].done.store(true, std::memory_order_release);
           workers[threadId].hasWork.store(false, std::memory_order_release);
+          workers[threadId].done.store(true, std::memory_order_release);
         }
       }
 
-      void _join(){
-        for (int i = 0; i < numThreads; i++) {
+      void _join(unsigned int activeThreads){
+        for (unsigned int i = 0; i < activeThreads; i++) {
           while (!workers[i].done.load(std::memory_order_acquire)) {
             std::this_thread::yield();
           }
         }
-        this->tempTaskData = nullptr;
-        this->tempFunct = nullptr;
-        this->tempFunctM = nullptr;
+        tempTaskData.store(nullptr, std::memory_order_relaxed);
+        tempFunct.store(nullptr, std::memory_order_relaxed);
+        tempFunctM.store(nullptr, std::memory_order_relaxed);
       }
+
+      inline void _setInfoTask(void* data, FunctPtr f1, FunctMPtr f2){
+        tempTaskData.store(data, std::memory_order_relaxed);
+        tempFunct.store(f1, std::memory_order_release);
+        tempFunctM.store(f2, std::memory_order_release);
+      }
+
+      inline void _waitForWorkerReady(unsigned int threadId) {
+        while (workers[threadId].hasWork.load(std::memory_order_acquire)) {
+          std::this_thread::yield();
+        }
+      }
+
+      inline void _notifyWorkers(unsigned int activeThreads) {
+        for (unsigned int i = 0; i < activeThreads; i++) {
+          workers[i].hasWork.store(true, std::memory_order_release);
+        }
+      }
+
     public:
 
       ParallelExecutor(unsigned int nThreads = std::thread::hardware_concurrency()){
@@ -58,6 +82,8 @@ namespace BitMth::core {
         workers = std::make_unique<Worker[]>(numThreads);
             
         for (unsigned int i = 0; i < numThreads; i++) {
+          workers[i].done.store(true, std::memory_order_relaxed);
+          workers[i].hasWork.store(false, std::memory_order_relaxed);
           workers[i].thr = std::thread(&ParallelExecutor::_workerLoop, this, i);
         }
       }
@@ -65,7 +91,7 @@ namespace BitMth::core {
       ~ParallelExecutor() {
         running.store(false, std::memory_order_relaxed);
         for (unsigned int i = 0; i < numThreads; i++) {
-          workers[i].hasWork.store(true, std::memory_order_release); // Despertamos para que vean el apagado
+          workers[i].hasWork.store(true, std::memory_order_release);
           if (workers[i].thr.joinable()) {
             workers[i].thr.join();
           }
@@ -76,30 +102,31 @@ namespace BitMth::core {
       template<typename T, typename Op>
       void execute(linalg::Matrix<T>& matrix, T scalar, Op funct){
         unsigned int size = matrix.size();
-        unsigned int parts = size / numThreads;
+        if (size == 0) return;
+        unsigned int activeThreads = (size < numThreads) ? size : numThreads;
+        unsigned int parts = size / activeThreads;
 
         TaskData<T,Op> data{matrix, scalar, funct};
-        this->tempTaskData = &data;
-        this->tempFunct = [](void* data, unsigned int start, unsigned int end){
-          auto* task = static_cast<TaskData<T, Op> *>(data);
-          task->funct(task->matrix, task->scalar, start, end);
-        };
 
+        _setInfoTask(
+          &data,
+          [](void* dataPtr, unsigned int start, unsigned int end) {
+            auto* task = static_cast<TaskData<T, Op>*>(dataPtr);
+            task->funct(task->matrix, task->scalar, start, end);
+          },
+          nullptr
+        );
 
-        for (int i = 0; i < numThreads; i++) {
+        for (unsigned int i = 0; i < activeThreads; i++) {
+          _waitForWorkerReady(i);
+
           workers[i].start = i * parts;
-          if(i == numThreads - 1){
-            workers[i].end = size;
-            workers[i].done.store(false, std::memory_order_relaxed);
-            workers[i].hasWork.store(true, std::memory_order_release);
-            continue;
-          }
-          workers[i].end = workers[i].start + parts;
+          workers[i].end = (i == activeThreads - 1) ? size : workers[i].start + parts;
           workers[i].done.store(false, std::memory_order_relaxed);
-          workers[i].hasWork.store(true, std::memory_order_release);
         }
 
-        _join();
+        _notifyWorkers(activeThreads);
+        _join(activeThreads);
       }
 
       template<typename T, typename Op>
@@ -111,37 +138,34 @@ namespace BitMth::core {
       void execute(linalg::Matrix<T>& matrixA, linalg::Matrix<T>& matrixB, Op funct){
         unsigned int totalRows = matrixA.getRows();
         unsigned int colEnd = matrixB.getCols();
+        if (totalRows == 0) return;
+        unsigned int activeThreads = (totalRows < numThreads) ? totalRows : numThreads;
 
-        unsigned int activeThreads = numThreads;
-        if (totalRows < activeThreads) {
-          activeThreads = totalRows;
-        }
-
-        if (activeThreads == 0) return;
         unsigned int parts = totalRows / activeThreads;
 
         TaskDataM<T,Op> data{matrixA, matrixB, funct};
-        this->tempTaskData = &data;
-        this->tempFunctM = [](void* data, unsigned int startR, unsigned int endR, unsigned int startC, unsigned int endC){
-          auto* task = static_cast<TaskDataM<T, Op> *>(data);
-          task->funct(task->matrixA, task->matrixB, startR, endR, startC, endC);
-        };
 
+        _setInfoTask(
+          &data,
+          nullptr,
+          [](void* dataPtr, unsigned int startR, unsigned int endR, unsigned int startC, unsigned int endC) {
+            auto* task = static_cast<TaskDataM<T, Op>*>(dataPtr);
+            task->funct(task->matrixA, task->matrixB, startR, endR, startC, endC);
+          }
+        );
 
-        for (int i = 0; i < numThreads; i++) {
-          workers[i].rowStart= i * parts;
+        for (unsigned int i = 0; i < activeThreads; i++) {
+          _waitForWorkerReady(i);
+
+          workers[i].rowStart = i * parts;
+          workers[i].rowEnd = (i == activeThreads - 1) ? totalRows : workers[i].rowStart + parts;
           workers[i].colStart = 0;
           workers[i].colEnd = colEnd;
-          if(i == numThreads - 1){
-            workers[i].rowEnd = matrixA.getRows();
-          }else{
-            workers[i].rowEnd = workers[i].rowStart + parts;
-          }
           workers[i].done.store(false, std::memory_order_relaxed);
-          workers[i].hasWork.store(true, std::memory_order_release);
         }
 
-        _join();
+        _notifyWorkers(activeThreads);
+        _join(activeThreads);
       }
 
       template<typename T, typename Op>
