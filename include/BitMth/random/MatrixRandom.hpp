1e5d21210fc3352a653806082988f559087b8a68
dsmr linux <dsmr19961@gmail.com>
2026-07-26 17:25:09 -0500

    refactor(MatrixRandom): optimize uniform and normal distribution functions using scalarApplyFunctionInPlace

--- include/BitMth/random/MatrixRandom.hpp
+++ include/BitMth/random/MatrixRandom.hpp
@@ -1,24 +1,37 @@
 #pragma once
 
 #include <random>
-#include <cmath>
 
 #include <BitMth/linalg/Matrix.hpp>
 
 namespace BitMth::random{
-    inline std::random_device rd;
-    inline std::mt19937 gen(rd());
+    inline thread_local std::mt19937 gen(std::random_device{}());
 
     template <typename T>
     void uniform(linalg::Matrix<T>& matrix, T min, T max){
-        std::uniform_real_distribution<T> dist(min, max);
-        for (size_t i = 0; i < matrix.size(); i++) matrix.getValues()[i] = dist(gen);
+        using DistType = std::conditional_t<
+            std::is_integral_v<T>,
+            std::uniform_int_distribution<T>,
+            std::uniform_real_distribution<T>
+        >;
+
+        DistType dist(min, max);
+        T unusedScalar = T(0);
+        matrix.scalarApplyFunctionInPlace(unusedScalar, [&dist](T& element, T) {
+            element = dist(gen);
+        });
     }
 
     template <typename T>
     void normal(linalg::Matrix<T>& matrix, T mean, T stddev){
+        static_assert(std::is_floating_point_v<T>,"normal only supports floating-point types (float, double, etc.).");
+
         std::normal_distribution<T> dist(mean, stddev);
-        for (size_t i = 0; i < matrix.size(); i++) matrix.getValues()[i] = dist(gen);
+        T unusedScalar = static_cast<T>(0);
+
+        matrix.scalarApplyFunctionInPlace(unusedScalar, [&dist](T& element, T) {
+            element = dist(gen);
+        });
     }
 
     template <typename T>
