0121979870d6589bbfaafecc185e457c1f3ffdd2
dsmr linux <dsmr19961@gmail.com>
2026-07-26 22:37:24 -0500

    feat(Loss): implement binary cross-entropy loss and its derivative, enhance MSE calculations

--- include/BitMth/ia/Loss.hpp
+++ include/BitMth/ia/Loss.hpp
@@ -6,8 +6,8 @@
 
 namespace BitMth::ia{
     enum class LossFunct: unsigned char {
-        MSE
-        // BINARY_CROSS_ENTROPY
+        MSE,
+        BINARY_CROSS_ENTROPY
         // CATEGORICAL_CROSS_ENTROPY
     };
 
@@ -18,41 +18,63 @@ namespace BitMth::ia{
             "Loss function (MSE)",
             "Matrix dimensions must match (rows = rows && cols == cols)"
         );
-        return (predict - real).pow(2).reduceSumTotal() / static_cast<T>(real.size());
+        linalg::Matrix<T> lossMatrix = linalg::Matrix<T>::matrixApplyFunction(
+            predict, real, nullptr, [](T mA, T mB) {
+                T diff = mA - mB;
+                return diff * diff;
+            }
+        );
+        return lossMatrix.reduceSumTotal() / static_cast<T>(real.size());
     }
 
     template <typename T>
     [[nodiscard]] linalg::Matrix<T> mseDerivative(const linalg::Matrix<T>& predict, const linalg::Matrix<T>& real){
-        return predict - real;
+        CHECK_ERROR_MATRIX(
+            predict.getRows() != real.getRows() || predict.getCols() != real.getCols(),
+            "Loss function (MSE derivative)",
+            "Matrix dimensions must match (rows == rows && cols == cols)"
+        );
+
+        const T invN = static_cast<T>(1) / static_cast<T>(real.size());
+        return linalg::Matrix<T>::matrixApplyFunction(
+            predict, real, nullptr, [invN](T mA, T mB) {
+                return (mA - mB) * invN;
+            }
+        );
     }
 
-    // template <typename T>
-    // [[nodiscard]] T bce(const linalg::Matrix<T>& predict, const linalg::Matrix<T>& real) {
-    //     CHECK_ERROR_MATRIX(
-    //         predict.getRows() != real.getRows() || predict.getCols() != real.getCols(),
-    //         "Loss function (BCE)",
-    //         "Matrix dimensions must match (rows == rows && cols == cols)"
-    //     );
+    template <typename T>
+    [[nodiscard]] T bce(const linalg::Matrix<T>& predict, const linalg::Matrix<T>& real) {
+        CHECK_ERROR_MATRIX(
+            predict.getRows() != real.getRows() || predict.getCols() != real.getCols(),
+            "Loss function (BCE)",
+            "Matrix dimensions must match (rows == rows && cols == cols)"
+        );
+        const T one = 1;
+        linalg::Matrix<T> lossMatrix = linalg::Matrix<T>::matrixApplyFunction(
+            predict, real, nullptr, [one](T mA, T mB){
+                T y_pred = std::clamp(mA, utils::EPSILON<T>, one - utils::EPSILON<T>);
+                return  mB * std::log(y_pred) + (one - mB) * std::log(one - y_pred);
+        });
+        return (-lossMatrix.reduceSumTotal()) / T(real.size());
+    }
 
-    //     linalg::Matrix<T> lossMatrix(predict.getRows(), predict.getCols());
+    template <typename T>
+    [[nodiscard]] linalg::Matrix<T> bceDerivative(const linalg::Matrix<T>& predict, const linalg::Matrix<T>& real) {
+        CHECK_ERROR_MATRIX(
+            predict.getRows() != real.getRows() || predict.getCols() != real.getCols(),
+            "Loss function (BCE derivative)",
+            "Matrix dimensions must match (rows == rows && cols == cols)"
+        );
+        const T one = static_cast<T>(1);
+        const T N = one / static_cast<T>(real.size());
 
-    //     for (size_t i = 0; i < predict.size(); i++) {
-    //         T y_pred = std::clamp(predict.getValues()[i], utils::EPSILON<T>, T(1.0) - utils::EPSILON<T>);
-    //         T y_real = real.getValues()[i];
-    //         lossMatrix.getValues()[i] = y_real * std::log(y_pred) + (T(1.0) - y_real) * std::log(T(1.0) - y_pred);
-    //     }
-    //     return -lossMatrix.reduceSumTotal() / static_cast<T>(real.size());
-    // }
-
-    // template <typename T>
-    // [[nodiscard]] linalg::Matrix<T> bceDerivative(const linalg::Matrix<T>& predict, const linalg::Matrix<T>& real) {
-    //     CHECK_ERROR_MATRIX(
-    //         predict.getRows() != real.getRows() || predict.getCols() != real.getCols(),
-    //         "Loss function (BCE derivative)",
-    //         "Matrix dimensions must match (rows == rows && cols == cols)"
-    //     );
-        
-    //     linalg::Matrix<T> denominator = (T(1.0) - predict).hadamardInPlace(predict);
-    //     return (predict - real) / (denominator + utils::EPSILON<T>) ;
-    // }
+        return linalg::Matrix<T>::matrixApplyFunction(
+            predict, real, nullptr, [one, N](T mA, T mB) {
+                T y_pred = std::clamp(mA, utils::EPSILON<T>, one - utils::EPSILON<T>);
+                T grad = (y_pred - mB) / (y_pred * (one - y_pred));
+                return grad * N;
+            }
+        );
+    }
 }
