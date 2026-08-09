0f78f3cbab5fb8d6dbbe48881291773d1cbfcafe
dsmr linux <dsmr19961@gmail.com>
2026-08-08 22:19:53 -0500

    refactor(LossFunctions): update mse and bce derivative functions to include gradScal parameter

--- include/BitMth/ia/Loss.hpp
+++ include/BitMth/ia/Loss.hpp
@@ -2,18 +2,10 @@
 #include <unordered_map>
 
 #include <BitMth/ia/LossFunctions.hpp>
-#include <BitMth/ia/autograd/Ops.hpp>
 
 namespace BitMth::ia{
     template<typename T>
-    inline const ia::types::LossContent<T>& getLossFunction(const ia::types::LossFunctType type, bool activateAutograd = false){
-        if(activateAutograd){
-            static std::unordered_map<ia::types::LossFunctType, ia::types::LossContent<T>> functTable ={
-                {ia::types::LossFunctType::MSE,                  {Ops<T>::mse, nullptr}},
-                {ia::types::LossFunctType::BINARY_CROSS_ENTROPY, {Ops<T>::bce, nullptr}}
-            };
-            return functTable.at(type);
-        }
+    inline const ia::types::LossContent<T>& getLossFunction(const ia::types::LossFunctType type){
         static std::unordered_map<ia::types::LossFunctType, ia::types::LossContent<T>> functTable ={
             {ia::types::LossFunctType::MSE,                  {LossFunctions<T>::mse, LossFunctions<T>::mseDerivative}},
             {ia::types::LossFunctType::BINARY_CROSS_ENTROPY, {LossFunctions<T>::bce, LossFunctions<T>::bceDerivative}}
--- include/BitMth/ia/LossFunctions.hpp
+++ include/BitMth/ia/LossFunctions.hpp
@@ -32,14 +32,14 @@ namespace BitMth::ia{
             return _helperMatrix(lossMatrix.reduceSumTotal() / static_cast<T>(real.size()),targetArena);
         }
 
-        static Matrix mseDerivative(const Matrix& predict, const Matrix& real, core::Arena* targetArena = nullptr){
+        static Matrix mseDerivative(const Matrix& predict, const Matrix& real,T gradScal = static_cast<T>(1), core::Arena* targetArena = nullptr){
             CHECK_ERROR_MATRIX(
                 predict.getRows() != real.getRows() || predict.getCols() != real.getCols(),
                 "Loss function (MSE derivative)",
                 "Matrix dimensions must match (rows == rows && cols == cols)"
             );
 
-            const T invN = static_cast<T>(1) / static_cast<T>(real.size());
+            const T invN = (T(2) * gradScal) / static_cast<T>(real.size());
             return Matrix::matrixApplyFunction(
                 predict, real, targetArena, [invN](T mA, T mB) {
                     return (mA - mB) * invN;
@@ -53,7 +53,7 @@ namespace BitMth::ia{
                 "Loss function (BCE)",
                 "Matrix dimensions must match (rows == rows && cols == cols)"
             );
-            const T one = 1;
+            constexpr T one = static_cast<T>(1);
             Matrix lossMatrix = Matrix::matrixApplyFunction(
                 predict, real, targetArena, [one](T mA, T mB){
                     T y_pred = std::clamp(mA, utils::EPSILON<T>, one - utils::EPSILON<T>);
@@ -62,14 +62,14 @@ namespace BitMth::ia{
             return _helperMatrix((-lossMatrix.reduceSumTotal()) / T(real.size()),targetArena);
         }
 
-        static Matrix bceDerivative(const Matrix& predict, const Matrix& real, core::Arena* targetArena = nullptr) {
+        static Matrix bceDerivative(const Matrix& predict, const Matrix& real,T gradScal = static_cast<T>(1), core::Arena* targetArena = nullptr) {
             CHECK_ERROR_MATRIX(
                 predict.getRows() != real.getRows() || predict.getCols() != real.getCols(),
                 "Loss function (BCE derivative)",
                 "Matrix dimensions must match (rows == rows && cols == cols)"
             );
-            const T one = static_cast<T>(1);
-            const T N = one / static_cast<T>(real.size());
+            constexpr T one = static_cast<T>(1);
+            const T N = gradScal / static_cast<T>(real.size());
 
             return Matrix::matrixApplyFunction(
                 predict, real, targetArena, [one, N](T mA, T mB) {
