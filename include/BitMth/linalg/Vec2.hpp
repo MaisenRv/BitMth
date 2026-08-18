20870bd4ae7b34c327aa871a4cada721c7a8e942
dsmr linux <dsmr19961@gmail.com>
2026-08-17 21:05:17 -0500

    refactor(Vec2): become class [[nodiscard]] and fix sub operation

--- include/BitMth/linalg/Vec2.hpp
+++ include/BitMth/linalg/Vec2.hpp
@@ -8,8 +8,8 @@
 
 namespace BitMth::linalg{
     template <typename T>
-    struct Vec2{
-        T x{}, y{};
+    class [[nodiscard]] Vec2{
+        T x{0}, y{0};
 
         Vec2(T x, T y): x(x), y(y){}
         Vec2() = default;
@@ -22,37 +22,41 @@ namespace BitMth::linalg{
         ~Vec2() = default;
 
         // Operators Vector - number
-        [[nodiscard]] Vec2<T> operator+(T number) const { return  Vec2<T>(x + number, y + number); }
-        [[nodiscard]] friend Vec2<T> operator+(T number, const Vec2<T>& vector) { return  vector + number; }
+        Vec2<T> operator+(T number) const { return  Vec2<T>(x + number, y + number); }
+        friend Vec2<T> operator+(T number, const Vec2<T>& vector) { return  vector + number; }
         Vec2<T>& operator+=(T number) { 
             x += number;
             y += number;
             return *this; 
         }
 
-        [[nodiscard]] Vec2<T> operator-(T number) const { return  Vec2<T>(x - number, y - number); }
-        [[nodiscard]] friend Vec2<T> operator-(T number, const Vec2<T>& vector) { return  vector - number; }
+        Vec2<T> operator-(T number) const { return  Vec2<T>(x - number, y - number); }
+        friend Vec2<T> operator-(T number, const Vec2<T>& vector) { return  Vec2<T>(number - vector.x, number - vector.y); }
         Vec2<T>& operator-=(T number) { 
             x -= number;
             y -= number;
             return *this; 
         }
 
-        [[nodiscard]] Vec2<T> operator*(T number) const { return  Vec2<T>(x * number, y * number); }
-        [[nodiscard]] friend Vec2<T> operator*(T number, const Vec2<T>& vector) { return  vector * number; }
+        Vec2<T> operator*(T number) const { return  Vec2<T>(x * number, y * number); }
+        friend Vec2<T> operator*(T number, const Vec2<T>& vector) { return  vector * number; }
         Vec2<T>& operator*=(T number) { 
             x *= number;
             y *= number;
             return *this; 
         }
 
-        [[nodiscard]] Vec2<T> operator/(T number){ 
-            CHECK_ERROR_VECTOR( number == T(0), "Vector division (/)", "Division for zero", "2d");
+        Vec2<T> operator/(T number) const{ 
+            CHECK_ERROR_VECTOR(
+                number < utils::EPSILON<T> || number > -utils::EPSILON<T>,
+                "Vector division (/)", "Division by zero", "2d");
             T inv = T(1) / number;
             return  Vec2<T>(x * inv, y * inv);
         }
         Vec2<T>& operator/=(T number) { 
-            CHECK_ERROR_VECTOR( number == T(0), "Vector division (/)", "Division for zero", "2d");
+            CHECK_ERROR_VECTOR(
+                number < utils::EPSILON<T> || number > -utils::EPSILON<T>
+                , "Vector division (/)", "Division by zero", "2d");
             T inv = T(1) / number;
             x *= inv;
             y *= inv;
@@ -60,23 +64,23 @@ namespace BitMth::linalg{
         }
 
         // Operators Vector - Vector
-        [[nodiscard]] Vec2<T> operator+(const Vec2<T>& vector) const { return Vec2<T>(x + vector.x, y + vector.y); }
+        Vec2<T> operator+(const Vec2<T>& vector) const { return Vec2<T>(x + vector.x, y + vector.y); }
         Vec2<T>& operator+=(const Vec2<T>& vector)  { 
             x += vector.x;
             y += vector.y;
             return *this;
         }
 
-        [[nodiscard]] Vec2<T> operator-(const Vec2<T>& vector) const { return Vec2<T>(x - vector.x, y - vector.y); }
+        Vec2<T> operator-(const Vec2<T>& vector) const { return Vec2<T>(x - vector.x, y - vector.y); }
         Vec2<T>& operator-=(const Vec2<T>& vector)  { 
             x -= vector.x;
             y -= vector.y;
             return *this;
         } 
 
-        [[nodiscard]] T dot(const Vec2<T>& vector)            const { return (x * vector.x) + (y * vector.y);}
-        [[nodiscard]] T cross (const Vec2<T>& vector)         const { return (x * vector.y) - (y * vector.x);}
-        [[nodiscard]] Vec2<T> hadamard(const Vec2<T>& vector) const { return Vec2<T>(x * vector.x, y * vector.y); }
+        T dot(const Vec2<T>& vector)            const { return (x * vector.x) + (y * vector.y);}
+        T cross (const Vec2<T>& vector)         const { return (x * vector.y) - (y * vector.x);}
+        Vec2<T> hadamard(const Vec2<T>& vector) const { return Vec2<T>(x * vector.x, y * vector.y); }
         Vec2<T>& hadamardInPlace(const Vec2<T>& vector){
             x *= vector.x;
             y *= vector.y;
@@ -84,17 +88,17 @@ namespace BitMth::linalg{
         }
 
         // Others
-        [[nodiscard]] T distanceSquared(const Vec2<T>& vector) const { 
+        T distanceSquared(const Vec2<T>& vector) const { 
             T dx = vector.x - x;
             T dy = vector.y - y;
             return (dx * dx) + (dy * dy);
         }
-        [[nodiscard]] T distance(const Vec2<T>& vector) const {
+        T distance(const Vec2<T>& vector) const {
             return std::sqrt(distanceSquared(vector));
         }
 
-        [[nodiscard]] T magnitudeSquared() const { return (x * x) + (y * y); }
-        [[nodiscard]] T magnitude() const { return std::sqrt(magnitudeSquared()); }
+        T magnitudeSquared() const { return (x * x) + (y * y); }
+        T magnitude() const { return std::sqrt(magnitudeSquared()); }
 
         Vec2<T>& normalizeInPlance(){
             T mag = magnitude();
@@ -105,23 +109,23 @@ namespace BitMth::linalg{
             x = T(0); y = T(0);
             return *this;
         }
-        [[nodiscard]] Vec2<T> normalize() const {
+        Vec2<T> normalize() const {
             T mag = magnitude();
             if (mag > utils::EPSILON<T>) return *this / mag; 
             return Vec2<T>(T(0), T(0));
         }
 
-        [[nodiscard]] T angleTo(const Vec2<T>& vector) const {
+        T angleTo(const Vec2<T>& vector) const {
             T dx = vector.x - x;
             T dy = vector.y - y;
             return std::atan2(dy, dx);
         }
 
-        [[nodiscard]] T angleBetween(const Vec2<T>& vector) const {
+        T angleBetween(const Vec2<T>& vector) const {
             return std::atan2(cross(vector), dot(vector));
         }
 
-        [[nodiscard]] Vec2<T> midpoint(const Vec2<T>& vector) const {
+        Vec2<T> midpoint(const Vec2<T>& vector) const {
             return (*this + vector) * T(0.5);
         }
 
